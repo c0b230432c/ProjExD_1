309dc98eb51661528f23fdc1577d9851f93041d0
kwtaym <c0b230432c@edu.teu.ac.jp>
2024-11-19 16:16:58 +0900

    7.背景ループ

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    fbg_img=pg.transform.flip(bg_img,True,False)
     kokaton=pg.image.load("fig/3.png")
     kokaton=pg.transform.flip(kokaton,True,False)
     tmr = 0
-    (width,height)=bg_img.get_size()
+    # (width,height)=bg_img.get_size()
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-(tmr%800), 0])
+        bg_start=-(tmr%3200)
+        screen.blit(bg_img, [bg_start, 0])
+        screen.blit(fbg_img,[bg_start+1600,0])
+        screen.blit(bg_img, [bg_start+3200, 0])
+        screen.blit(fbg_img,[bg_start+4800,0])
         screen.blit(kokaton,[300,200])
         pg.display.update()
         tmr += 1        
